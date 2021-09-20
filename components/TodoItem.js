b46d30a99202cc3da11455488cbd60e2a0ba6d27
Archit Soni <69715914+ArchitSoni07@users.noreply.github.com>
2021-09-20 11:10:20 +0530

    Add files via upload

---
+++ components/TodoItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text,TouchableOpacity,View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function TodoItem({item,pressHandler}){
+    return(
+        <TouchableOpacity onPress={()=>pressHandler(item.key)}>
+            <View style={styles.text}>
+            <AntDesign name="delete" size={24} color="black" />
+            <Text style={styles.itemText}>{item.text}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    text:{
+        padding:14,
+        marginTop:16,
+        borderColor:'#bbb',
+        borderWidth:1,
+        borderStyle:'dashed',
+        borderRadius:10,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    itemText:{
+        marginLeft:10,
+        fontSize:18
+    }
+})
